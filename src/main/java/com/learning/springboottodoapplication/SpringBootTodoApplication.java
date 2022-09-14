509905a24d90b017cef4a48338265cc9182697ac
Gautam Parmar <gautamparmar201@gmail.com>
2022-09-14 14:07:13 +0200

    templete implementation and component scan

--- src/main/java/com/learning/springboottodoapplication/SpringBootTodoApplication.java
+++ src/main/java/com/learning/springboottodoapplication/SpringBootTodoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@ComponentScan(basePackages = {"com.learning.springboottodoapplication.controllers","com.learning.springboottodoapplication.config",
+		"com.learning.springboottodoapplication.models","com.learning.springboottodoapplication.repositories"})
 public class SpringBootTodoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/learning/springboottodoapplication/repositories/TodoItemRepository.java
+++ src/main/java/com/learning/springboottodoapplication/repositories/TodoItemRepository.java
@@ -1,5 +1,4 @@
-package com.repositories;
-
+package com.learning.springboottodoapplication.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.learning.springboottodoapplication.models.TodoItem;
